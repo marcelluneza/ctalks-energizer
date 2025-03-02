76ed999f0a8e2b1f1c57ba8ab3944e6d92b8fc44
marcelluneza <mgluneza@up.edu.ph>
2025-03-03 02:27:18 +0800

    refactor: moved scripts to single js file

--- static/script.js
+++ static/script.js
@@ -35,3 +35,31 @@ document.getElementById('runButton').addEventListener('click', function () {
             console.error('Error:', error);
         });
 });
+
+
+document.getElementById('search-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const searchWord = document.getElementById('word').value.toLowerCase().trim();
+
+    let found = false;
+    let cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        const cardWord = card.querySelector('h3').textContent;
+
+        if (cardWord === searchWord) {
+            card.classList.replace("covered", "uncovered");
+            found = true;
+        }
+    });
+
+    if (!found) {
+        document.getElementById('no-result').classList.remove('hidden');
+        setTimeout(function () {
+            document.getElementById('no-result').classList.add('hidden');
+
+        }, 2000);
+
+    }
+});
